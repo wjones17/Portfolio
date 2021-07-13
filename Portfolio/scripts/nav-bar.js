50121e2ebf3df6ec8d9d61d72df27e192faaab5a
Tyler Jones <76069841+wjones17@users.noreply.github.com>
2021-07-13 10:49:40 -0400

    initial

---
+++ Portfolio/scripts/nav-bar.js
@@ -0,0 +1,10 @@
+Vue.component('nav-bar', {
+  template:`
+ <div id="navigation">
+    <ul>
+      <li><a href="home.html">Home</a></li>
+      <li><a href="hobbies.html">Hobbies</a></li>
+      <li><a href="resume.html">Resume</a></li>
+    </ul>
+  </div>`
+});
