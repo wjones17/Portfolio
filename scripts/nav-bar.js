3f2e454430d76c617febb90b3eb929d565af2e80
Tyler Jones <76069841+wjones17@users.noreply.github.com>
2024-03-09 14:12:59 -0500

    Update nav-bar.js

--- scripts/nav-bar.js
+++ scripts/nav-bar.js
@@ -2,7 +2,7 @@ Vue.component('nav-bar', {
   template:`
  <div id="navigation">
     <ul>
-      <li><a href="home.html">Home</a></li>
+      <li><a href="index.html">Home</a></li>
       <li><a href="hobbies.html">Hobbies</a></li>
       <li><a href="resume.html">Resume</a></li>
     </ul>
