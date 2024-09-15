624c531b6ff5a37ec6c1cb5e3d4006207837c956
William jones <wtjracer13@yahoo.com>
2024-09-15 10:14:34 -0400

    adding new files

---
+++ scripts/app.js
@@ -0,0 +1,25 @@
+var wrapper = new Vue({
+  el: "#wrapper",
+  data: {
+    info: {
+      firstName: "William",
+      lastName: "Jones",
+      email: "wtjracer13@yahoo.com"
+    },
+    education: {
+      id: 1,
+      name: "University Of West Georgia",
+      degree: "Bachelor of Science in Computer Science",
+      year: "Graduated: May 2023"
+    },
+    showEducation: false
+  },
+  methods: {
+    changeEducation: function () {
+      this.showEducation = !this.showEducation;
+    },
+    changeTechnology: function () {
+      this.showTechnology = !this.showTechnology;
+    },
+  }
+});
---
+++ scripts/nav-bar.js
@@ -0,0 +1,10 @@
+Vue.component('nav-bar', {
+  template:`
+ <div id="navigation">
+    <ul>
+      <li><a href="index.html">Home</a></li>
+      <li><a href="hobbies.html">Hobbies</a></li>
+      <li><a href="resume.html">Resume</a></li>
+    </ul>
+  </div>`
+});
